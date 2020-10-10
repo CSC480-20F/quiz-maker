ce72a9309d535e45bb31e434627c8e0942f63fac
Anisha KC <anisha.kc7219@gmail.com>
2020-10-10 17:59:06 -0400

    csv parsed with header

--- Frontend/src/main/frontend/src/components/CreateQuiz.js
+++ Frontend/src/main/frontend/src/components/CreateQuiz.js
@@ -114,6 +114,14 @@ import { CSVReader } from 'react-papaparse';
 const buttonRef = React.createRef()
 
 class CreateQuiz extends React.Component {
+    constructor(props) {
+            super(props);
+            this.state = {
+                "response": [],
+                "emails": []
+            }
+        }
+
   handleOpenDialog = (e) => {
     // Note that the ref is set async, so it might be null at some point
     if (buttonRef.current) {
@@ -121,10 +129,22 @@ class CreateQuiz extends React.Component {
     }
   }
 
-  handleOnFileLoad = (data) => {
-    console.log('---------------------------')
-    console.log(data)
-    console.log('---------------------------')
+  handleOnDrop = (data) => {
+    console.log('---------------------------');
+    console.log(data);
+    console.log('---------------------------');
+    this.setState({
+      "response": data
+    })
+    console.log(this.state.response);
+    this.state.response.map((data) => {
+      var joined = this.state.myArray.concat(data.Emails);
+      this.setState({ emails: joined })
+    }
+  }
+  
+  getEmails = () => {
+    
   }
 
   handleOnError = (err, file, inputElem, reason) => {
@@ -149,67 +169,15 @@ class CreateQuiz extends React.Component {
       <>
         <TopNavbar/>
         <div className="container">
-        <h5>Basic Upload</h5>
+        <h5>Click and Drag Upload</h5>
         <CSVReader
-          ref={buttonRef}
-          onFileLoad={this.handleOnFileLoad}
+          config={{header: true}}
+          onDrop={this.handleOnDrop}
           onError={this.handleOnError}
-          noClick
-          noDrag
+          addRemoveButton
           onRemoveFile={this.handleOnRemoveFile}
         >
-          {({ file }) => (
-            <aside
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                marginBottom: 10
-              }}
-            >
-              <button
-                type='button'
-                onClick={this.handleOpenDialog}
-                style={{
-                  borderRadius: 0,
-                  marginLeft: 0,
-                  marginRight: 0,
-                  width: '40%',
-                  paddingLeft: 0,
-                  paddingRight: 0
-                }}
-              >
-                Browse file
-              </button>
-              <div
-                style={{
-                  borderWidth: 1,
-                  borderStyle: 'solid',
-                  borderColor: '#ccc',
-                  height: 45,
-                  lineHeight: 2.5,
-                  marginTop: 5,
-                  marginBottom: 5,
-                  paddingLeft: 13,
-                  paddingTop: 3,
-                  width: '60%'
-                }}
-              >
-                {file && file.name}
-              </div>
-              <button
-                style={{
-                  borderRadius: 0,
-                  marginLeft: 0,
-                  marginRight: 0,
-                  paddingLeft: 20,
-                  paddingRight: 20
-                }}
-                onClick={this.handleRemoveFile}
-              >
-                Remove
-              </button>
-            </aside>
-          )}
+          <span>Drop CSV file here or click to upload.</span>
         </CSVReader>
         </div>
       </>
