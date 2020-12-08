6b2494f9413a44a7c141b2abe936d8d9a0971c9a
Matt Ljuljic <70350971+mljuljicPrograms@users.noreply.github.com>
2020-12-08 00:46:24 -0500

    Final Commit
    Last small files required to launch the servers

---
+++ serverStart.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+cd /home/mljuljic/Csc480/quiz-maker/Frontend/
+nohup mvn process-resources liberty:run &  
+cd ../Users-microservice/
+nohup mvn liberty:run & 
+cd ../Courses-microservice
+nohup mvn liberty:run &
+cd ../Quizzes-microservice
+nohup mvn liberty:run & 
+cd
+cd ~/mongo
+nohup mongod --bind_ip_all -f mongod.conf & 
+nohup mongod --bind_ip_all -f coursesMongod.conf & 
+nohup mongod --bind_ip_all -f quizesMongod.conf & 
